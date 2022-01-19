10a9df0ea03bd0549a4378c3d8d45bb5a7a1b279
Restyled.io <commits@restyled.io>
2022-01-19 11:36:27 +0000

    Restyled by astyle

--- kernel/task_work.c
+++ kernel/task_work.c
@@ -30,35 +30,35 @@ static struct callback_head work_exited; /* all we need is ->next == NULL */
  * 0 if succeeds or -ESRCH.
  */
 int task_work_add(struct task_struct *task, struct callback_head *work,
-		  enum task_work_notify_mode notify)
+                  enum task_work_notify_mode notify)
 {
-	struct callback_head *head;
+    struct callback_head *head;
 
-	/* record the work call stack in order to print it in KASAN reports */
-	kasan_record_aux_stack(work);
+    /* record the work call stack in order to print it in KASAN reports */
+    kasan_record_aux_stack(work);
 
-	do {
-		head = READ_ONCE(task->task_works);
-		if (unlikely(head == &work_exited))
-			return -ESRCH;
-		work->next = head;
-	} while (cmpxchg(&task->task_works, head, work) != head);
+    do {
+        head = READ_ONCE(task->task_works);
+        if (unlikely(head == &work_exited))
+            return -ESRCH;
+        work->next = head;
+    } while (cmpxchg(&task->task_works, head, work) != head);
 
-	switch (notify) {
-	case TWA_NONE:
-		break;
-	case TWA_RESUME:
-		set_notify_resume(task);
-		break;
-	case TWA_SIGNAL:
-		set_notify_signal(task);
-		break;
-	default:
-		WARN_ON_ONCE(1);
-		break;
-	}
+    switch (notify) {
+    case TWA_NONE:
+        break;
+    case TWA_RESUME:
+        set_notify_resume(task);
+        break;
+    case TWA_SIGNAL:
+        set_notify_signal(task);
+        break;
+    default:
+        WARN_ON_ONCE(1);
+        break;
+    }
 
-	return 0;
+    return 0;
 }
 EXPORT_SYMBOL(task_work_add);
 
@@ -72,36 +72,36 @@ EXPORT_SYMBOL(task_work_add);
  */
 struct callback_head *
 task_work_cancel_match(struct task_struct *task,
-		       bool (*match)(struct callback_head *, void *data),
-		       void *data)
+                       bool (*match)(struct callback_head *, void *data),
+                       void *data)
 {
-	struct callback_head **pprev = &task->task_works;
-	struct callback_head *work;
-	unsigned long flags;
+    struct callback_head **pprev = &task->task_works;
+    struct callback_head *work;
+    unsigned long flags;
 
-	if (likely(!task->task_works))
-		return NULL;
-	/*
-	 * If cmpxchg() fails we continue without updating pprev.
-	 * Either we raced with task_work_add() which added the
-	 * new entry before this work, we will find it again. Or
-	 * we raced with task_work_run(), *pprev == NULL/exited.
-	 */
-	raw_spin_lock_irqsave(&task->pi_lock, flags);
-	while ((work = READ_ONCE(*pprev))) {
-		if (!match(work, data))
-			pprev = &work->next;
-		else if (cmpxchg(pprev, work, work->next) == work)
-			break;
-	}
-	raw_spin_unlock_irqrestore(&task->pi_lock, flags);
+    if (likely(!task->task_works))
+        return NULL;
+    /*
+     * If cmpxchg() fails we continue without updating pprev.
+     * Either we raced with task_work_add() which added the
+     * new entry before this work, we will find it again. Or
+     * we raced with task_work_run(), *pprev == NULL/exited.
+     */
+    raw_spin_lock_irqsave(&task->pi_lock, flags);
+    while ((work = READ_ONCE(*pprev))) {
+        if (!match(work, data))
+            pprev = &work->next;
+        else if (cmpxchg(pprev, work, work->next) == work)
+            break;
+    }
+    raw_spin_unlock_irqrestore(&task->pi_lock, flags);
 
-	return work;
+    return work;
 }
 
 static bool task_work_func_match(struct callback_head *cb, void *data)
 {
-	return cb->func == data;
+    return cb->func == data;
 }
 
 /**
@@ -118,7 +118,7 @@ static bool task_work_func_match(struct callback_head *cb, void *data)
 struct callback_head *
 task_work_cancel(struct task_struct *task, task_work_func_t func)
 {
-	return task_work_cancel_match(task, task_work_func_match, func);
+    return task_work_cancel_match(task, task_work_func_match, func);
 }
 
 /**
@@ -131,40 +131,40 @@ task_work_cancel(struct task_struct *task, task_work_func_t func)
  */
 void task_work_run(void)
 {
-	struct task_struct *task = current;
-	struct callback_head *work, *head, *next;
+    struct task_struct *task = current;
+    struct callback_head *work, *head, *next;
 
-	for (;;) {
-		/*
-		 * work->func() can do task_work_add(), do not set
-		 * work_exited unless the list is empty.
-		 */
-		do {
-			head = NULL;
-			work = READ_ONCE(task->task_works);
-			if (!work) {
-				if (task->flags & PF_EXITING)
-					head = &work_exited;
-				else
-					break;
-			}
-		} while (cmpxchg(&task->task_works, work, head) != work);
+    for (;;) {
+        /*
+         * work->func() can do task_work_add(), do not set
+         * work_exited unless the list is empty.
+         */
+        do {
+            head = NULL;
+            work = READ_ONCE(task->task_works);
+            if (!work) {
+                if (task->flags & PF_EXITING)
+                    head = &work_exited;
+                else
+                    break;
+            }
+        } while (cmpxchg(&task->task_works, work, head) != work);
 
-		if (!work)
-			break;
-		/*
-		 * Synchronize with task_work_cancel(). It can not remove
-		 * the first entry == work, cmpxchg(task_works) must fail.
-		 * But it can remove another entry from the ->next list.
-		 */
-		raw_spin_lock_irq(&task->pi_lock);
-		raw_spin_unlock_irq(&task->pi_lock);
+        if (!work)
+            break;
+        /*
+         * Synchronize with task_work_cancel(). It can not remove
+         * the first entry == work, cmpxchg(task_works) must fail.
+         * But it can remove another entry from the ->next list.
+         */
+        raw_spin_lock_irq(&task->pi_lock);
+        raw_spin_unlock_irq(&task->pi_lock);
 
-		do {
-			next = work->next;
-			work->func(work);
-			work = next;
-			cond_resched();
-		} while (work);
-	}
+        do {
+            next = work->next;
+            work->func(work);
+            work = next;
+            cond_resched();
+        } while (work);
+    }
 }
