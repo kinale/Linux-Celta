1689de59f5460111c090075a76b02dccae0e9b13
Restyled.io <commits@restyled.io>
2022-01-19 13:57:16 +0000

    Restyled by clang-format

--- include/uapi/linux/futex.h
+++ include/uapi/linux/futex.h
@@ -7,53 +7,51 @@
 
 /* Second argument to futex syscall */
 
+#define FUTEX_WAIT 0
+#define FUTEX_WAKE 1
+#define FUTEX_FD 2
+#define FUTEX_REQUEUE 3
+#define FUTEX_CMP_REQUEUE 4
+#define FUTEX_WAKE_OP 5
+#define FUTEX_LOCK_PI 6
+#define FUTEX_UNLOCK_PI 7
+#define FUTEX_TRYLOCK_PI 8
+#define FUTEX_WAIT_BITSET 9
+#define FUTEX_WAKE_BITSET 10
+#define FUTEX_WAIT_REQUEUE_PI 11
+#define FUTEX_CMP_REQUEUE_PI 12
+#define FUTEX_LOCK_PI2 13
+#define FUTEX_WAIT_MULTIPLE 31
 
-#define FUTEX_WAIT		0
-#define FUTEX_WAKE		1
-#define FUTEX_FD		2
-#define FUTEX_REQUEUE		3
-#define FUTEX_CMP_REQUEUE	4
-#define FUTEX_WAKE_OP		5
-#define FUTEX_LOCK_PI		6
-#define FUTEX_UNLOCK_PI		7
-#define FUTEX_TRYLOCK_PI	8
-#define FUTEX_WAIT_BITSET	9
-#define FUTEX_WAKE_BITSET	10
-#define FUTEX_WAIT_REQUEUE_PI	11
-#define FUTEX_CMP_REQUEUE_PI	12
-#define FUTEX_LOCK_PI2		13
-#define FUTEX_WAIT_MULTIPLE		31
+#define FUTEX_PRIVATE_FLAG 128
+#define FUTEX_CLOCK_REALTIME 256
+#define FUTEX_CMD_MASK ~(FUTEX_PRIVATE_FLAG | FUTEX_CLOCK_REALTIME)
 
-#define FUTEX_PRIVATE_FLAG	128
-#define FUTEX_CLOCK_REALTIME	256
-#define FUTEX_CMD_MASK		~(FUTEX_PRIVATE_FLAG | FUTEX_CLOCK_REALTIME)
-
-#define FUTEX_WAIT_PRIVATE	(FUTEX_WAIT | FUTEX_PRIVATE_FLAG)
-#define FUTEX_WAKE_PRIVATE	(FUTEX_WAKE | FUTEX_PRIVATE_FLAG)
-#define FUTEX_REQUEUE_PRIVATE	(FUTEX_REQUEUE | FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAIT_PRIVATE (FUTEX_WAIT | FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAKE_PRIVATE (FUTEX_WAKE | FUTEX_PRIVATE_FLAG)
+#define FUTEX_REQUEUE_PRIVATE (FUTEX_REQUEUE | FUTEX_PRIVATE_FLAG)
 #define FUTEX_CMP_REQUEUE_PRIVATE (FUTEX_CMP_REQUEUE | FUTEX_PRIVATE_FLAG)
-#define FUTEX_WAKE_OP_PRIVATE	(FUTEX_WAKE_OP | FUTEX_PRIVATE_FLAG)
-#define FUTEX_LOCK_PI_PRIVATE	(FUTEX_LOCK_PI | FUTEX_PRIVATE_FLAG)
-#define FUTEX_LOCK_PI2_PRIVATE	(FUTEX_LOCK_PI2 | FUTEX_PRIVATE_FLAG)
-#define FUTEX_UNLOCK_PI_PRIVATE	(FUTEX_UNLOCK_PI | FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAKE_OP_PRIVATE (FUTEX_WAKE_OP | FUTEX_PRIVATE_FLAG)
+#define FUTEX_LOCK_PI_PRIVATE (FUTEX_LOCK_PI | FUTEX_PRIVATE_FLAG)
+#define FUTEX_LOCK_PI2_PRIVATE (FUTEX_LOCK_PI2 | FUTEX_PRIVATE_FLAG)
+#define FUTEX_UNLOCK_PI_PRIVATE (FUTEX_UNLOCK_PI | FUTEX_PRIVATE_FLAG)
 #define FUTEX_TRYLOCK_PI_PRIVATE (FUTEX_TRYLOCK_PI | FUTEX_PRIVATE_FLAG)
-#define FUTEX_WAIT_BITSET_PRIVATE	(FUTEX_WAIT_BITSET | FUTEX_PRIVATE_FLAG)
-#define FUTEX_WAKE_BITSET_PRIVATE	(FUTEX_WAKE_BITSET | FUTEX_PRIVATE_FLAG)
-#define FUTEX_WAIT_REQUEUE_PI_PRIVATE	(FUTEX_WAIT_REQUEUE_PI | \
-					 FUTEX_PRIVATE_FLAG)
-#define FUTEX_CMP_REQUEUE_PI_PRIVATE	(FUTEX_CMP_REQUEUE_PI | \
-					 FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAIT_BITSET_PRIVATE (FUTEX_WAIT_BITSET | FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAKE_BITSET_PRIVATE (FUTEX_WAKE_BITSET | FUTEX_PRIVATE_FLAG)
+#define FUTEX_WAIT_REQUEUE_PI_PRIVATE                                          \
+	(FUTEX_WAIT_REQUEUE_PI | FUTEX_PRIVATE_FLAG)
+#define FUTEX_CMP_REQUEUE_PI_PRIVATE (FUTEX_CMP_REQUEUE_PI | FUTEX_PRIVATE_FLAG)
 
 /*
  * Flags to specify the bit length of the futex word for futex2 syscalls.
  * Currently, only 32 is supported.
  */
-#define FUTEX_32		2
+#define FUTEX_32 2
 
 /*
  * Max numbers of elements in a futex_waitv array
  */
-#define FUTEX_WAITV_MAX		128
+#define FUTEX_WAITV_MAX 128
 
 /**
  * struct futex_waitv - A waiter for vectorized wait
@@ -63,10 +61,10 @@
  * @__reserved:	Reserved member to preserve data alignment. Should be 0.
  */
 struct futex_waitv {
-    __u64 val;
-    __u64 uaddr;
-    __u32 flags;
-    __u32 __reserved;
+	__u64 val;
+	__u64 uaddr;
+	__u32 flags;
+	__u32 __reserved;
 };
 
 /**
@@ -76,9 +74,9 @@ struct futex_waitv {
  * @bitset:	Bitset for the optional bitmasked wakeup
  */
 struct futex_wait_block {
-    __u32 __user *uaddr;
-    __u32 val;
-    __u32 bitset;
+	__u32 __user *uaddr;
+	__u32 val;
+	__u32 bitset;
 };
 
 /*
@@ -96,7 +94,7 @@ struct futex_wait_block {
  * changed.
  */
 struct robust_list {
-    struct robust_list __user *next;
+	struct robust_list __user *next;
 };
 
 /*
@@ -108,20 +106,20 @@ struct robust_list {
  *  size, which glibc will detect)
  */
 struct robust_list_head {
-    /*
+	/*
      * The head of the list. Points back to itself if empty:
      */
-    struct robust_list list;
+	struct robust_list list;
 
-    /*
+	/*
      * This relative offset is set by user-space, it gives the kernel
      * the relative position of the futex field to examine. This way
      * we keep userspace flexible, to freely shape its data-structure,
      * without hardcoding any particular offset into the kernel:
      */
-    long futex_offset;
+	long futex_offset;
 
-    /*
+	/*
      * The death of the thread may race with userspace setting
      * up a lock's links. So to handle this race, userspace first
      * sets this field to the address of the to-be-taken lock,
@@ -131,13 +129,13 @@ struct robust_list_head {
      * _might_ have taken. We check the owner TID in any case,
      * so only truly owned locks will be handled.
      */
-    struct robust_list __user *list_op_pending;
+	struct robust_list __user *list_op_pending;
 };
 
 /*
  * Are there any waiters for this robust futex:
  */
-#define FUTEX_WAITERS		0x80000000
+#define FUTEX_WAITERS 0x80000000
 
 /*
  * The kernel signals via this bit that a thread holding a futex
@@ -145,40 +143,39 @@ struct robust_list_head {
  * a FUTEX_WAKE on such futexes, after setting the bit, to wake
  * up any possible waiters:
  */
-#define FUTEX_OWNER_DIED	0x40000000
+#define FUTEX_OWNER_DIED 0x40000000
 
 /*
  * The rest of the robust-futex field is for the TID:
  */
-#define FUTEX_TID_MASK		0x3fffffff
+#define FUTEX_TID_MASK 0x3fffffff
 
 /*
  * This limit protects against a deliberately circular list.
  * (Not worth introducing an rlimit for it)
  */
-#define ROBUST_LIST_LIMIT	2048
+#define ROBUST_LIST_LIMIT 2048
 
 /*
  * bitset with all bits set for the FUTEX_xxx_BITSET OPs to request a
  * match of any bit.
  */
-#define FUTEX_BITSET_MATCH_ANY	0xffffffff
+#define FUTEX_BITSET_MATCH_ANY 0xffffffff
 
+#define FUTEX_OP_SET 0 /* *(int *)UADDR2 = OPARG; */
+#define FUTEX_OP_ADD 1 /* *(int *)UADDR2 += OPARG; */
+#define FUTEX_OP_OR 2 /* *(int *)UADDR2 |= OPARG; */
+#define FUTEX_OP_ANDN 3 /* *(int *)UADDR2 &= ~OPARG; */
+#define FUTEX_OP_XOR 4 /* *(int *)UADDR2 ^= OPARG; */
 
-#define FUTEX_OP_SET		0	/* *(int *)UADDR2 = OPARG; */
-#define FUTEX_OP_ADD		1	/* *(int *)UADDR2 += OPARG; */
-#define FUTEX_OP_OR		2	/* *(int *)UADDR2 |= OPARG; */
-#define FUTEX_OP_ANDN		3	/* *(int *)UADDR2 &= ~OPARG; */
-#define FUTEX_OP_XOR		4	/* *(int *)UADDR2 ^= OPARG; */
+#define FUTEX_OP_OPARG_SHIFT 8 /* Use (1 << OPARG) instead of OPARG.  */
 
-#define FUTEX_OP_OPARG_SHIFT	8	/* Use (1 << OPARG) instead of OPARG.  */
-
-#define FUTEX_OP_CMP_EQ		0	/* if (oldval == CMPARG) wake */
-#define FUTEX_OP_CMP_NE		1	/* if (oldval != CMPARG) wake */
-#define FUTEX_OP_CMP_LT		2	/* if (oldval < CMPARG) wake */
-#define FUTEX_OP_CMP_LE		3	/* if (oldval <= CMPARG) wake */
-#define FUTEX_OP_CMP_GT		4	/* if (oldval > CMPARG) wake */
-#define FUTEX_OP_CMP_GE		5	/* if (oldval >= CMPARG) wake */
+#define FUTEX_OP_CMP_EQ 0 /* if (oldval == CMPARG) wake */
+#define FUTEX_OP_CMP_NE 1 /* if (oldval != CMPARG) wake */
+#define FUTEX_OP_CMP_LT 2 /* if (oldval < CMPARG) wake */
+#define FUTEX_OP_CMP_LE 3 /* if (oldval <= CMPARG) wake */
+#define FUTEX_OP_CMP_GT 4 /* if (oldval > CMPARG) wake */
+#define FUTEX_OP_CMP_GE 5 /* if (oldval >= CMPARG) wake */
 
 /* FUTEX_WAKE_OP will perform atomically
    int oldval = *(int *)UADDR2;
@@ -186,8 +183,8 @@ struct robust_list_head {
    if (oldval CMP CMPARG)
      wake UADDR2;  */
 
-#define FUTEX_OP(op, oparg, cmp, cmparg) \
-  (((op & 0xf) << 28) | ((cmp & 0xf) << 24)		\
-   | ((oparg & 0xfff) << 12) | (cmparg & 0xfff))
+#define FUTEX_OP(op, oparg, cmp, cmparg)                                       \
+	(((op & 0xf) << 28) | ((cmp & 0xf) << 24) | ((oparg & 0xfff) << 12) |  \
+	 (cmparg & 0xfff))
 
 #endif /* _UAPI_LINUX_FUTEX_H */
